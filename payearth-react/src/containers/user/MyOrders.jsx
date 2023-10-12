c9e420560fe767bf68f430fe8ca735aeba9891b3
upendrapanwar <upendrapanwar456@gmail.com>
2023-10-12 18:23:15 +0530

    Update files React + Nodejs 12-10-2023

--- payearth-react/src/containers/user/MyOrders.jsx
+++ payearth-react/src/containers/user/MyOrders.jsx
@@ -4,6 +4,7 @@ import PageTitle from './../../components/user/common/PageTitle';
 import Footer from './../../components/common/Footer';
 import store from './../../store/index';
 import axios from 'axios';
+
 import { toast } from 'react-toastify';
 import { connect } from 'react-redux';
 import { setLoading } from './../../store/reducers/global-reducer';
@@ -11,18 +12,22 @@ import { Link } from 'react-router-dom';
 import SpinnerLoader from './../../components/common/SpinnerLoader';
 import NotFound from './../../components/common/NotFound';
 import Select from 'react-select';
-import DataTable from 'react-data-table-component'; 
+import DataTable from 'react-data-table-component';
+import DataTableExtensions from "react-data-table-component-extensions"; 
+import "react-data-table-component-extensions/dist/index.css";
 
 class MyOrders extends Component {
     constructor(props) {
         super(props);
         const { dispatch } = props;
+        
         this.dispatch = dispatch;
         this.authInfo = store.getState().auth.authInfo;
         this.state = {
             data: [],
+
             orderDataSet:'',
-            columns:'',
+            columns:[],
             reqBody: {
                 count: {
                     page: 1,
@@ -47,41 +52,10 @@ class MyOrders extends Component {
         this.getOrders(false, null);
     }
 
+    
     getOrders = (pagination, param) => {
+        
         let reqBody = {};
-        var orderData = '';
-        let columnsArray = [
-            {
-                name: 'id',
-                selector: row => row.id,
-                cell: row => <h3>{row.id}</h3>,
-                sortable: true,
-            },
-            {
-                name: 'invoice_number',
-                selector: row => row.invoice_number,
-                cell: row => <h3>{row.invoice_number}</h3>,
-                sortable: true,
-            },
-            {
-                name: 'payment_mode',
-                selector: row => row.payment_mode,
-                cell: row => <h3>{row.payment_mode}</h3>,
-                sortable: true,
-            },
-            {
-                name: 'amount',
-                selector: row => row.amount,
-                cell: row => <h3>{row.amount}</h3>,
-                sortable: true,
-            },
-            {
-                name: 'order_status',
-                selector: row => row.order_status,
-                cell: row => <h3>{row.order_status}</h3>,
-                sortable: true,
-            }
-        ];
         if (pagination === true) {
             reqBody = {
                 count: {
@@ -106,16 +80,56 @@ class MyOrders extends Component {
                 'Authorization': `Bearer ${this.authInfo.token}`
             }
         }).then((response) => {
-            console.log('data=',response.data.data.orders);
-            console.log('status=',response.data.status);
+            
             if (response.data.status) {
-                orderData = this.getDatatableData(response.data.data.orders);
-                columnsArray = JSON.stringify(columnsArray, null, 2);
+                const columnsData = [
+                    {
+                      name: 'ID',
+                      selector: (row, i) => row.id,
+                      cell: (row) => <span>{row.id}</span>,
+                      sortable: true,
+                    },
+                    {
+                      name: 'Invoice Number',
+                      selector: (row, i) => row.invoice_number,
+                      cell: (row) => <span>{row.invoice_number}</span>,
+                      sortable: true,
+                    },
+                    {
+                      name: 'Payment Mode',
+                      selector: (row, i) => row.payment_mode,
+                      cell: (row) => <span>{row.payment_mode}</span>,
+                      sortable: true,
+                    },
+                    {
+                      name: 'Amount',
+                      selector: (row, i) => row.amount,
+                      cell: (row) => <span>${row.amount}</span>,
+                      sortable: true,
+                    },
+                    {
+                      name: 'Order Status',
+                      selector: (row, i) => row.order_status,
+                      cell: (row) => <span>{row.order_status}</span>,
+                      sortable: true,
+                    },
+                     {
+                         name: 'Actions',
+                         cell: (row) => (
+                             <>
+                                 <button
+                                     onClick={() => this.handleOrderReciept(row._id)}
+                                     className="custom_btn btn_yellow_bordered w-auto btn"
+                                 >
+                                     Download
+                                 </button>
+                             </>
+                         ),
+                     }
+                  ];
                 this.setState({
-                    columns: columnsArray,
-                    orderDataSet: orderData,
-                    data: response.data.data.orders,
-                    pagination: response.data.data.paginationData
+                    orderDataSet: this.getDatatableData(response.data.data.orders),
+                    columns: columnsData,
                 });
                     
                 
@@ -131,6 +145,31 @@ class MyOrders extends Component {
         });
     }
 
+    handleOrderReciept = (id) => {
+        /*axios.post('user/orders/' + id, reqBody, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Authorization': `Bearer ${this.authInfo.token}`
+            }
+        }).then((response) => {
+            
+            if (response.data.status) {
+                
+                
+                    
+                
+            }
+        }).catch(error => {
+            if(error.response && error.response.data.status === false) {
+                toast.error(error.response.data.message);
+            }
+        }).finally(() => {
+            setTimeout(() => {
+                this.dispatch(setLoading({loading: false}));
+            }, 300);
+        });*/
+    }
     pagination = () => {
         let html = [];
         for (let index = 0; index < this.state.pagination.totalPages; index++) {
@@ -139,12 +178,11 @@ class MyOrders extends Component {
         }
         return html;
     }
-    
     getDatatableData = (orderDataObj) => {
         
         let orderDataArray = [];
         orderDataObj.forEach(function(value) {
-            //console.log('value',value);
+            
             orderDataArray.push({
                 id:value.orderCode,
                 invoice_number: value.paymentId.invoiceNo,
@@ -153,38 +191,18 @@ class MyOrders extends Component {
                 order_status:value.orderStatus.orderStatusId.title
             });
             
-            //console.log('orderDataArray',orderDataArray);
         })
-        return JSON.stringify(orderDataArray, null, 2);
-        
-    }
-
-    handleChange = selectedOption => {
-        let reqBody = this.state.reqBody;
-        reqBody.sorting.sort_val = selectedOption.value;
-        reqBody.count.page = this.state.pagination.currentPage;
-        reqBody.count.skip = (this.state.pagination.currentPage - 1) * 3;
-        this.setState({defaultSelectedOption: selectedOption, reqBody});
-        this.getOrders(false, this.state.pagination.currentPage);
-    }
-
-    getAllData = () => {
-        
-        setTimeout(() => {
-            
-            return (
-                <DataTable
-                    columns={this.state.columns}
-                    data={this.state.orderDataSet}
-
-                />
-            );
-        }, 1000);
+        return orderDataArray;
         
     }
 
     render() {
-        const { data, pagination, sortingOptions, defaultSelectedOption } = this.state;
+
+        const tableData = {
+            columns: this.state.columns,
+            data: this.state.orderDataSet
+        };
+        
         const { loading } = store.getState().global;
         
         return (
@@ -199,78 +217,21 @@ class MyOrders extends Component {
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="cart wishlist">
-                                    <div className="cart_wrap">
-                                        <div className="items_incart">
-                                            <span className="text-uppercase">{pagination.totalOrders ? pagination.totalOrders : 0} ITEMS IN YOUR ORDERS</span>
-                                            
-                                            <Select
-                                                className="sort_select text-normal"
-                                                options={sortingOptions}
-                                                value={defaultSelectedOption}
-                                                onChange={this.handleChange}
-                                                isDisabled={data.length === 0 ? true : false}
-                                            />
-                                        </div>
-                                    </div>
+                                    
                                     <div className="cart_list cart_wrap pb-5">
-                                        
-                                        {setTimeout(() => {
-                                            var datas = this.state.orderDataSet;
-                                            if(datas.length) {
-                                                console.log('columns',this.state.columns);
-                                                console.log('data',this.state.orderDataSet);
-                                                <DataTable
-                                                    title="Table"
-                                                    columns={this.state.columns}
-                                                    data={this.state.orderDataSet}
-                                                    noDataComponent="Your Text Here"
-                                                    pagination
-                                                    
-                                                    
-                                                />
-                                            }
-                                        }, 2000)}
-                                        {/*
-                                        {data.length > 0 ?
-                                            <table className="table table-responsive table-hover pe_table">
-                                                <thead>
-                                                    <tr>
-                                                        <th>ORDER ID</th>
-                                                        <th>INVOICE NUMBER</th>
-                                                        <th>PAYMENT MODE</th>
-                                                        <th>AMOUNT</th>
-                                                        <th>ORDER STATUS</th>
-                                                        
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr><td></td></tr>
-                                                    {data.length && data.map((value, index) => {
-                                                        return  <tr key={index}>
-                                                                    <td>{value.orderCode}</td>
-                                                                    <td>{value.paymentId.invoiceNo}</td>
-                                                                    <td>{value.paymentId.paymentAccount}</td>
-                                                                    <td>${value.paymentId.amountPaid}</td>
-                                                                    <td>{value.orderStatus.orderStatusId.title}</td>
-                                                                    
-                                                                    <td><Link to={`/order-detail/${value.id}`} className="btn custom_btn btn_yellow_bordered">View Details</Link></td>
-                                                                </tr>
-                                                    })}
-                                                </tbody>
-                                            </table>
-                                        : <NotFound msg="Data not found." />}
-
-                                        {data.length > 0 &&
-                                            <div className="pagination">
-                                                <ul>
-                                                    <li><Link to="#" className={`link ${pagination.hasPrevPage ? '' : 'disabled'}`} onClick={() => this.getOrders(true, pagination.prevPage)}><span className="fa fa-angle-left me-2"></span> Prev</Link></li>
-                                                    {this.pagination()}
-                                                    <li><Link to="#" className={`link ${pagination.hasNextPage ? '' : 'disabled'}`} onClick={() => this.getOrders(true, pagination.nextPage)}>Next <span className="fa fa-angle-right ms-2"></span></Link></li>
-                                                </ul>
-                                            </div>
-                                        }
-                                        */}
+                                    
+                                        <DataTableExtensions {...tableData}>
+                                            <DataTable
+                                                title="Table"
+                                                columns={this.state.columns}
+                                                data={this.state.orderDataSet}
+                                                noHeader
+                                                defaultSortField="id"
+                                                defaultSortAsc={false}
+                                                pagination
+                                                highlightOnHover
+                                            />
+                                        </DataTableExtensions>
                                     </div>
                                 </div>
                             </div>
