f34e4b447de228d705c06b03cf14d038bbae0db2
DESKTOP-VGJ2J0K\Ashish Mishra <ashishindia2493@gmail.com>
2023-10-31 18:34:55 +0530

    Corrected header's info alert closing error

--- payearth-react/src/components/user/home/InfoAlert.jsx
+++ payearth-react/src/components/user/home/InfoAlert.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const InfoAlert = () => {
-    return(
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleClose = () => {
+        setIsVisible(false);
+    }
+
+    return isVisible ? (
         <div className="offer_alert alert alert-danger alert-dismissible fade show mb-0 rounded-0" role="alert">
             <div className="text_wrapper">
                 <span className="wow">Woww !!</span>
@@ -10,9 +16,9 @@ const InfoAlert = () => {
                 <span className="dont_miss">Don’t<br /><i>Miss this</i></span>
                 <Link to="#" className="btn custom_btn btn_yellow_bordered">Start to earn now</Link>
             </div>
-            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <button type="button" className="btn-close" onClick={handleClose} aria-label="Close"></button>
         </div>
-    )
+    ) : null;
 }
 
 export default InfoAlert;
